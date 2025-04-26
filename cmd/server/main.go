c8863529d96b26c2dce173308f5c3ca795e0b4ad
Erich Hafenmaier <ehafenmaier@gmail.com>
2025-04-26 10:30:32 -0700

    Create channel and publish message in server main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"os"
 	"os/signal"
@@ -19,6 +21,24 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Successfully connected to RabbitMQ")
+
+	// Create a channel
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Printf("Failed to open a channel: %s\n", err)
+		return
+	}
+	defer ch.Close()
+
+	// Publish a message
+	msg := routing.PlayingState{
+		IsPaused: true,
+	}
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, msg)
+	if err != nil {
+		fmt.Printf("Failed to publish message: %s\n", err)
+		return
+	}
 
 	// Wait for Ctrl+C signal
 	signalChan := make(chan os.Signal, 1)
