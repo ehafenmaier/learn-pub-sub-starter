d54d106308881839a39a1027d7dbd82c0c244f3d
Erich Hafenmaier <ehafenmaier@gmail.com>
2025-04-26 10:32:40 -0700

    Declare and bind a transient queue in the peril client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	// Connect to RabbitMQ
+	connStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		fmt.Printf("Failed to connect to RabbitMQ: %s\n", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Println("Successfully connected to RabbitMQ")
+
+	// Prompt user for username
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Printf("Failed to get username: %s\n", err)
+		return
+	}
+
+	// Declare and bind the queue
+	ch, q, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		routing.PauseKey+"."+username,
+		routing.PauseKey,
+		pubsub.TransientQueue)
+	if err != nil {
+		fmt.Printf("Failed to declare and bind queue: %s\n", err)
+		return
+	}
+
+	// Print channel and queue information
+	fmt.Printf("Channel: %v\n", ch)
+	fmt.Printf("Queue: %v\n", q)
+
+	// Wait for Ctrl+C signal
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Shutting down Peril client...")
 }
