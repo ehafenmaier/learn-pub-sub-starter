57c33f6d8fb9cbd9fc3138275029ab05c0b4674c
Erich Hafenmaier <ehafenmaier@gmail.com>
2025-05-11 17:59:32 -0700

    Move war handler function to its own file

--- cmd/client/war.go
+++ cmd/client/war.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
-	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
-func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.AckType {
-	return func(move gamelogic.ArmyMove) pubsub.AckType {
+func handlerWar(gs *gamelogic.GameState) func(rw gamelogic.RecognitionOfWar) pubsub.AckType {
+	return func(rw gamelogic.RecognitionOfWar) pubsub.AckType {
 		defer fmt.Print("> ")
 
 		// Connect to RabbitMQ
@@ -20,19 +19,22 @@ func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.AckTyp
 		defer conn.Close()
 		defer ch.Close()
 
-		outcome := gs.HandleMove(move)
+		outcome, _, _ := gs.HandleWar(rw)
 		var ackType pubsub.AckType
 
 		switch outcome {
-		case gamelogic.MoveOutComeSafe:
-			ackType = pubsub.Ack
-		case gamelogic.MoveOutcomeMakeWar:
-			key := routing.WarRecognitionsPrefix + "." + gs.GetUsername()
-			pubsub.PublishJSON(ch, routing.ExchangePerilTopic, key, move)
+		case gamelogic.WarOutcomeNotInvolved:
 			ackType = pubsub.NackRequeue
-		case gamelogic.MoveOutcomeSamePlayer:
+		case gamelogic.WarOutcomeNoUnits:
 			ackType = pubsub.NackDiscard
+		case gamelogic.WarOutcomeOpponentWon:
+			ackType = pubsub.Ack
+		case gamelogic.WarOutcomeYouWon:
+			ackType = pubsub.Ack
+		case gamelogic.WarOutcomeDraw:
+			ackType = pubsub.Ack
 		default:
+			fmt.Println("Error: Unknown war outcome")
 			ackType = pubsub.NackDiscard
 		}
 
