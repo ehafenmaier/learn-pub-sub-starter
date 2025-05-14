8320c0151c23ed1cefe8a4b3575ef064211eb8cd
Erich Hafenmaier <ehafenmaier@gmail.com>
2025-05-13 21:51:50 -0700

    Refactor move and war handler functions

--- cmd/client/move.go
+++ cmd/client/move.go
@@ -5,41 +5,35 @@ import (
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.AckType {
+func handlerMove(gs *gamelogic.GameState, ch *amqp.Channel) func(gamelogic.ArmyMove) pubsub.AckType {
 	return func(move gamelogic.ArmyMove) pubsub.AckType {
 		defer fmt.Print("> ")
 
-		// Connect to RabbitMQ
-		conn, ch, err := pubsub.ConnectRabbitMQ()
-		if err != nil {
-			fmt.Printf("Failed to connect to RabbitMQ: %s\n", err)
-			return pubsub.NackDiscard
-		}
-		defer conn.Close()
-		defer ch.Close()
-
 		outcome := gs.HandleMove(move)
-		var ackType pubsub.AckType
 
 		switch outcome {
 		case gamelogic.MoveOutComeSafe:
-			ackType = pubsub.Ack
+			return pubsub.Ack
+		case gamelogic.MoveOutcomeSamePlayer:
+			return pubsub.Ack
 		case gamelogic.MoveOutcomeMakeWar:
 			key := routing.WarRecognitionsPrefix + "." + gs.GetUsername()
-			err = pubsub.PublishJSON(ch, routing.ExchangePerilTopic, key, move)
-			if err != nil {
-				ackType = pubsub.NackRequeue
-			} else {
-				ackType = pubsub.Ack
+			rw := gamelogic.RecognitionOfWar{
+				Attacker: move.Player,
+				Defender: gs.GetPlayerSnap(),
 			}
-		case gamelogic.MoveOutcomeSamePlayer:
-			ackType = pubsub.NackDiscard
-		default:
-			ackType = pubsub.NackDiscard
+			err := pubsub.PublishJSON(ch, routing.ExchangePerilTopic, key, rw)
+			if err != nil {
+				fmt.Printf("Failed to publish message: %s\n", err)
+				return pubsub.NackRequeue
+			}
+			return pubsub.Ack
 		}
 
-		return ackType
+		fmt.Println("Error: Unknown move outcome")
+		return pubsub.NackDiscard
 	}
 }
--- cmd/client/war.go
+++ cmd/client/war.go
@@ -4,40 +4,40 @@ import (
 	"fmt"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func handlerWar(gs *gamelogic.GameState) func(rw gamelogic.RecognitionOfWar) pubsub.AckType {
+func handlerWar(gs *gamelogic.GameState, ch *amqp.Channel) func(rw gamelogic.RecognitionOfWar) pubsub.AckType {
 	return func(rw gamelogic.RecognitionOfWar) pubsub.AckType {
 		defer fmt.Print("> ")
 
-		// Connect to RabbitMQ
-		conn, ch, err := pubsub.ConnectRabbitMQ()
-		if err != nil {
-			fmt.Printf("Failed to connect to RabbitMQ: %s\n", err)
-			return pubsub.NackDiscard
-		}
-		defer conn.Close()
-		defer ch.Close()
+		// Publish game log gob
+		//err := pubsub.PublishGob(
+		//	ch,
+		//	routing.ExchangePerilTopic,
+		//	routing.GameLogSlug+"."+rw.Attacker.Username,
+		//	rw)
+		//if err != nil {
+		//	fmt.Printf("Failed to publish game log: %s\n", err)
+		//	return pubsub.NackRequeue
+		//}
 
 		outcome, _, _ := gs.HandleWar(rw)
-		var ackType pubsub.AckType
 
 		switch outcome {
 		case gamelogic.WarOutcomeNotInvolved:
-			ackType = pubsub.NackRequeue
+			return pubsub.NackRequeue
 		case gamelogic.WarOutcomeNoUnits:
-			ackType = pubsub.NackDiscard
+			return pubsub.NackDiscard
 		case gamelogic.WarOutcomeOpponentWon:
-			ackType = pubsub.Ack
+			return pubsub.Ack
 		case gamelogic.WarOutcomeYouWon:
-			ackType = pubsub.Ack
+			return pubsub.Ack
 		case gamelogic.WarOutcomeDraw:
-			ackType = pubsub.Ack
-		default:
-			fmt.Println("Error: Unknown war outcome")
-			ackType = pubsub.NackDiscard
+			return pubsub.Ack
 		}
 
-		return ackType
+		fmt.Println("Error: Unknown war outcome")
+		return pubsub.NackDiscard
 	}
 }
